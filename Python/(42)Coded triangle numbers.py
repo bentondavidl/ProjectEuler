c6b790bd7ac0cb5efc44e17b040ba0b22329943a
David <bentondavidl@users.noreply.github.com>
2021-01-13 16:17:50 -0500

    Solve 42

---
+++ Python/(42)Coded triangle numbers.py
@@ -0,0 +1,16 @@
+triangle_nums = [int(.5*i*(i+1)) for i in range(1, 21)]
+
+
+with open('data files/p042_words.txt') as f:
+    words = f.read().replace('"', '').split(',')
+
+count = 0
+for word in words:
+    total = 0
+    for l in word:
+        total += ord(l) - 64
+
+    if total in triangle_nums:
+        count += 1
+
+print(count)
